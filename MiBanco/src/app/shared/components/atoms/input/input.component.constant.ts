ad4b6ec66c9cca2b524ec754708322c520c1b1e7
Harold Portillo <harold.portillo@delfosti.com>
2025-08-24 02:59:24 -0500

    feat update inputs

--- MiBanco/src/app/shared/components/atoms/input/input.component.constant.ts
+++ MiBanco/src/app/shared/components/atoms/input/input.component.constant.ts
@@ -1,6 +1,5 @@
 import { CommonModule } from "@angular/common";
 import { ReactiveFormsModule } from "@angular/forms";
-import { MatError, MatInputModule } from "@angular/material/input";
 import { NumberRangeDirective } from "@app/shared/directives/numberRange/numberRange.directive";
 import { TrimInputDirective } from '@app/shared/directives/trimInput/trimInput.directive';
 import { UppercaseDirective } from "@app/shared/directives/upperCase/upperCase.directive";
@@ -8,8 +7,6 @@ import { UppercaseDirective } from "@app/shared/directives/upperCase/upperCase.d
 export const INPUT_FORM_IMPORTS = [
     CommonModule,
     ReactiveFormsModule,
-    MatInputModule,
-    MatError,
     TrimInputDirective,
     NumberRangeDirective,
     UppercaseDirective
